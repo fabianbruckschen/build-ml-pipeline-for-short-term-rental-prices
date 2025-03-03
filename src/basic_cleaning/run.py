64bb0445fc6d81e68246582333c5620a8e4fe4f4
Fabian Bruckschen <fabian.bruckschen@gmail.com>
2025-03-03 20:46:39 +0100

    limit obs by geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -28,13 +28,16 @@ def go(args):
 
     df = pd.read_csv(artifact_path)
 
-    # Drop the duplicates
     logger.info("Dropping outliers")
     idx = df["price"].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
     logger.info("Converting last_review to datetime")
     df["last_review"] = pd.to_datetime(df["last_review"])
+
+    logger.info("Dropping rows in the dataset that are not in the proper geolocation for NYC")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     df.to_csv(args.output_artifact, index=False)
 
